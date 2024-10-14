f9ea9e4e83238536a204155d23b35597ffc99b27
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-10-15 00:20:44 +0600

    day 8

---
+++ day 8 function call by input.py
@@ -0,0 +1,24 @@
+#function call
+
+def my_function():
+     a='Do fast before it\'s too late,'
+     b='life is very short to argue,'
+     c='so let\'s do it now.'
+     print (a,b,c)
+
+my_function()
+  
+#function with input
+
+def dream(name):
+    print(f"Hello, {name}!")
+    print(f"Welcome to the dream world!")
+    print(f"You are a {name} in this world!")
+
+dream('Devops')
+
+#function with more than 1 input
+def greet(name, age):
+    print(f"Hello, {name}!")
+    print(f"You are {age} years old.")
+greet('Devops', 25)
---
+++ day 8 function call.py
@@ -0,0 +1,8 @@
+def my_function():
+    a='Do fast before it\'s too late,'
+    b='life is very short to argue,'
+    c='so let\'s do it now.'
+    print (a,b,c)
+
+my_function()
+
