2055ca97f9a60b895d507c2131becb1e9667228e
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-10-17 23:27:34 +0600

    day 10 project

--- Day 10 Calculator project/day 10 calculator project.py
+++ Day 10 Calculator project/day 10 calculator project.py
@@ -26,7 +26,18 @@ operations={
 }
 
 num1=int(input("what's the first number?: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol=input('Pick an operation right above:')
 num2=int(input("what's the second number?: "))
 
-for symbol in operations:
-    print(f"{num1} {symbol} {num2} = {operations[symbol]})
+calculation_function=operations[operation_symbol]
+answer=calculation_function(num1,num2)
+
+print(f'{num1}{operation_symbol}{num2}={answer}')
+
+operation_symbol=input('Pick an another operation:')
+num3=int(input("what's the second number?: "))
+calculation_function=operations[operation_symbol]
+second_answer=calculation_function(answer,num3)
+print(f'{answer}{operation_symbol}{num3}={second_answer}')
