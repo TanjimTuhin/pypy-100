7e9c68d180fd5af54db127e9f0dc7cc046e78fca
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-10-27 20:57:28 +0600

    day 16

--- day 16 oop.py
+++ day 16 oop.py
@@ -1,12 +1,18 @@
 from turtle import Turtle, Screen
 
-tom=Turtle()
+# what it has -> Attribute //object.atrribute ex.>my_screen.canvheight
+# what it can do -> Method //object.method()  ex.>tom.fd(50)
+
+tom=Turtle() #object=class
 print(tom)
 tom.shape("turtle")
 tom.color("red", "green")
-# tom.distance()
-tom.fd(50)
-
+tom.fd(50)   #forward/fd     # tom.distance() ->object method
+tom.right(45)
+tom.bk(75)   #backward/bk
+tom.circle(1)
+tom.fd(100)
+tom.lt(90)   #left/lt
 
 my_screen= Screen()
 print(my_screen.canvheight)  #(object.attribute)
