c9f4cbca95713d71c0c307f79cf664206426a628
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-10-27 20:38:22 +0600

    day 16

---
+++ day 16 oop.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+tom=Turtle()
+print(tom)
+tom.shape("turtle")
+tom.color("red", "green")
+# tom.distance()
+tom.fd(50)
+
+
+my_screen= Screen()
+print(my_screen.canvheight)  #(object.attribute)
+
+#object methods 
+my_screen.exitonclick()
+  
