9a347601fc1b17859c296772e3ce5b893567937c
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-10-30 23:37:10 +0600

    practice

--- day 17 quiz project.py
+++ day 17 quiz project.py
@@ -1,8 +1,13 @@
 class User:
-    pass
+    def __init__(self):
+        
 
 user_1 = User()
 user_1.id='001'
 user_1.username='Yuhan'
 
 print(user_1.username)
+
+user_2=User()
+user_2.id='002'
+user_2.username='Frankestine'
---
+++ practice .py
@@ -0,0 +1,11 @@
+def checkOddEven(x):
+    if(x % 2 == 0):
+      # Complete the statement below
+      print("Even")
+      return 
+    else:
+        # Complete the statement below
+        print("Odd")
+        return 
+    
+checkOddEven(int(input()))
