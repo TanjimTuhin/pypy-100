4c7b9f9c32271688d960b3226f9cc9bfbfec0be9
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-11-02 01:50:10 +0600

    random

--- practice .py
+++ practice .py
@@ -1,11 +1,12 @@
-def checkOddEven(x):
-    if(x % 2 == 0):
-      # Complete the statement below
-      print("Even")
-      return 
-    else:
-        # Complete the statement below
-        print("Odd")
-        return 
-    
-checkOddEven(int(input()))
+import array
+
+# Creating an array of integers
+#my_array = array.array('i', [1, 2, 3, 4])
+my_array = array.array(int(input()))
+
+# Accessing elements
+print(my_array[1])  # Output: 2
+
+# Modifying the array
+my_array.append(5)
+print(my_array)  # Output: array('i', [1, 2, 3, 4, 5])
