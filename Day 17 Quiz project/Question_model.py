f0c85cd27d7253fbaf64d07792db91ac4154591a
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-11-11 13:00:10 +0600

    17

--- Day 17 Quiz project/Question_model.py
+++ Day 17 Quiz project/Question_model.py
@@ -3,3 +3,10 @@ class Question:
         self.text = q_text
         self.answer = q_answer
 
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number=0
+        self.question_list=q_list
+    def next_question(self): 
+        current_question=self.question_list[self.question_number]
+               
--- Day 17 Quiz project/day 17 Quiz project.py
+++ Day 17 Quiz project/day 17 Quiz project.py
@@ -1,6 +1,15 @@
 from Question_model import Question
 from data import question_data
-
+#-------------------------------------------
+# question_data = [
+# {"text": "A slug's blood is green.", "answer": "True"},
+# {"text": "The loudest animal is the African Elephant.", "answer": "False"}]
+# #-------------------------------------------
+# class Question:
+#     def __init__(self, q_text, q_answer):
+#         self.text = q_text
+#         self.answer = q_answer
+#-------------------------------------------        
 question_bank=[]
 for question in question_data:
     question_text= question["text"]
@@ -9,3 +18,6 @@ for question in question_data:
     question_bank.append(new_question) 
 
 print(question_bank[0].answer)
+
+
+
